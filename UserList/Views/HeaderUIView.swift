515f764b265d7dfb6ca7454c5a749ab81bfba1d9
Patrick Aloueichek <p.aloueichek@gmail.com>
2019-09-20 13:16:23 +0200

    Fix HeaderUIView for better handling of emaillabel and nameLabel

--- UserList/Views/HeaderUIView.swift
+++ UserList/Views/HeaderUIView.swift
@@ -12,6 +12,47 @@ class HeaderUIView: UIView {
     
     var user: User
     
+   private lazy var nameLabel: UILabel = {
+        let nameLabel = UILabel()
+        nameLabel.font = .systemFont(ofSize: 20, weight: .light)
+        return nameLabel
+    }()
+    
+   private lazy var emailLabel: UILabel = {
+        let emailLabel = UILabel()
+        emailLabel.font = .systemFont(ofSize: 20, weight: .light)
+        emailLabel.numberOfLines = 0
+        emailLabel.sizeToFit()
+        return emailLabel
+    }()
+    
+   private lazy var phoneLabel: UILabel = {
+        let phoneLabel = UILabel()
+        phoneLabel.font = .systemFont(ofSize: 20, weight: .light)
+        phoneLabel.numberOfLines = 0
+        phoneLabel.sizeToFit()
+        return phoneLabel
+    }()
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "applover")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+  private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        stackView.spacing = 3
+        stackView.addArrangedSubview(self.nameLabel)
+        stackView.addArrangedSubview(self.emailLabel)
+        stackView.addArrangedSubview(self.phoneLabel)
+        return stackView
+    }()
+    
       init(frame: CGRect, user: User) {
         self.user = user
         super.init(frame: frame)
@@ -19,40 +60,19 @@ class HeaderUIView: UIView {
     }
     
     func setupImage() {
-        let nameLabel = UILabel()
-        let emailLabel = UILabel()
-        let phoneLabel = UILabel()
+        self.nameLabel.text = user.name
+        self.emailLabel.text = user.email
+        self.phoneLabel.text = user.phone
+ 
+        self.imageView.frame = frame
+        self.addSubview(self.imageView)
+        self.imageView.addSubview(stackView)
         
-        let view = UIView(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height / 2.0))
-       
-        let stackView = UIStackView(arrangedSubviews: [nameLabel,emailLabel,phoneLabel])
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        let imageView = UIImageView()
-        
-        stackView.distribution = .fillEqually
-        stackView.axis = .vertical
-        stackView.spacing = 3
-        
-        nameLabel.text = user.name
-        emailLabel.text = user.email
-        phoneLabel.text = user.phone
-        
-        nameLabel.font = .systemFont(ofSize: 20, weight: .light)
-        emailLabel.font = .systemFont(ofSize: 14, weight: .light)
-        phoneLabel.font = .systemFont(ofSize: 14, weight: .light)
-        
-        imageView.frame = frame
-        imageView.image = UIImage(named: "applover")
-        imageView.contentMode = .scaleAspectFit
-        imageView.addSubview(stackView)
-        self.addSubview(imageView)
-        
-        stackView.addSubview(view)
         NSLayoutConstraint.activate([
-            stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor,constant: 25),
-            stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-            stackView.widthAnchor.constraint(equalToConstant: 200),
-            stackView.heightAnchor.constraint(equalToConstant: 100)
+            self.stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor,constant: 25),
+            self.stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            self.stackView.widthAnchor.constraint(equalToConstant: 200),
+            self.stackView.heightAnchor.constraint(equalToConstant: 100)
             ])
     }
 
